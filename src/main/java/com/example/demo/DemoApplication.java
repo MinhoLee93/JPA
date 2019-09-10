4ba7e25c95a13456fef507f539af7374e22b5e8c
Minho Lee <alsgkgk77@gmail.com>
2019-09-10 23:22:15 +0900

    ch02 - EntityManager & Persistence

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactoryBuilder emfb    = new EntityManagerFactoryBuilder();
+        Entity
+
+        SpringApplication.run(DemoApplication.class, args);
+
+    }
+}
---
+++ src/main/java/com/example/demo/Member.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+public class Member {
+
+    // 안먹이히는 이유 확인 필요
+
+    @Entity
+    @Table(name="MEMBER")
+    public class Member {
+
+        @Id
+        @Column(name = "ID")
+        private String id;
+
+        @Column(name = "NAME")
+        private String username;
+
+        private Integer age;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+    }
+}
